9fcf7e84d070dc18a373fd1c79d25fc0a3ece108
asiimwedismas <asiimwedismas@gmail.com>
2020-05-27 01:11:49 +0300

    Fromatted document

--- bubble.rb
+++ bubble.rb
@@ -1,33 +1,28 @@
-def bubble_sort (arr)
-
-  (arr.length).times do 
-        (arr.length - 1).times do |i|
-            if arr[i] > arr [i+1]
-                arr[i], arr[i+1] = arr[i+1], arr[i]
-                i += 1
-            end
-        end            
-    end
- print arr   
-end 
-
-arr = [10, 5, 8, 11, 100, 8, 3]
-bubble_sort (arr)
-
-
-def bubble_sort_by (arr)
-  (arr.length).times do
-        (arr.length - 1).times do |i|
-            result = yield arr[i], arr[i+1]
-            if result.positive?
-                arr[i], arr[i+1] = arr[i+1], arr[i] 
-            end
+def bubble_sort(arr)
+  arr.length.times do
+    (arr.length - 1).times do |i|
+      if arr[i] > arr [i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        i += 1
+      end
     end
   end
- print arr
+  print arr
 end
 
+arr = [10, 5, 8, 11, 100, 8, 3]
+bubble_sortarr
 
-bubble_sort_by(["hiiiiii", "hello", "hey",]) do |left, right| 
-    left.length - right.length
+def bubble_sort_by(arr)
+  arr.length.times do
+    (arr.length - 1).times do |i|
+      result = yield arr[i], arr[i + 1]
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if result.positive?
+    end
+  end
+  print arr
+end
+
+bubble_sort_by(['hiiiiii', 'hello', 'hey']) do |left, right|
+  left.length - right.length
 end
