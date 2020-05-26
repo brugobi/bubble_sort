59dc1c409de03fa28dcb024e5df17d98b14751a8
asiimwedismas <asiimwedismas@gmail.com>
2020-05-27 00:04:35 +0300

    Added bubble_sort_by function

--- bubble.rb
+++ bubble.rb
@@ -8,9 +8,22 @@ def bubble_sort (arr)
             end
         end            
     end
-    print arr
-#    return (arr)    
+ print arr   
 end 
 
 arr = [10, 5, 8, 11, 100, 8, 3]
 bubble_sort (arr)
+
+
+def bubble_sort_by (arr)
+  (arr.length).times do
+        (arr.length - 1).times do |i|
+            result = yield arr[i], arr[i+1]
+      arr[i], arr[i+1] = arr[i+1], arr[i] if result.positive?
+    end
+  end
+ print arr
+end
+
+
+bubble_sort_by(["hiiiiii", "hello", "hey",]) do |left, right| left.length <=> right.length end
