65891d9b02d6b73de78708272e3ccbec3a22e672
brugobi <brgobbi@hotmail.com>
2020-05-26 15:50:21 -0400

    set up the function to sort an array and run

--- bubble.rb
+++ bubble.rb
@@ -1,14 +1,16 @@
 def bubble_sort (arr)
-    
-    arr = [4, 2, 1, 4, 5]
 
-  2.times do 
-        1.times do |i|
+  (arr.length).times do 
+        (arr.length - 1).times do |i|
             if arr[i] > arr [i+1]
                 arr[i], arr[i+1] = arr[i+1], arr[i]
                 i += 1
             end
         end            
-    end    
+    end
+    print arr
+#    return (arr)    
+end 
 
-end    
+arr = [10, 5, 8, 11, 100, 8, 3]
+bubble_sort (arr)
