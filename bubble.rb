09466c0bff74edc4dc2a60b42e4bb53d05216733
brugobi <brgobbi@hotmail.com>
2020-05-26 15:41:55 -0400

    created a function that sort n elemnets in an array

--- bubble.rb
+++ bubble.rb
@@ -1,13 +1,14 @@
 def bubble_sort (arr)
     
-    arr = [4, 2]
+    arr = [4, 2, 1, 4, 5]
 
-    1.times do |i|
-        if arr[i] > arr [i+1]
-            arr[i], arr[i+1] = arr[i+1], arr[i]
-            i += 1
-        end
-    end            
-
+  2.times do 
+        1.times do |i|
+            if arr[i] > arr [i+1]
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                i += 1
+            end
+        end            
+    end    
 
 end    
