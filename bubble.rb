29de17e9d85cb0358e28d3110d4cd557ebfbd65d
asiimwedismas <asiimwedismas@gmail.com>
2020-05-27 18:02:30 +0300

    Added a test file to hold all the tests

--- bubble.rb
+++ bubble.rb
@@ -1,16 +1,11 @@
 def bubble_sort(arr)
   arr.length.times do
     (arr.length - 1).times do |i|
-      if arr[i] > arr [i + 1]
-        arr[i], arr[i + 1] = arr[i + 1], arr[i]
-      end
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i] > arr [i + 1]
     end
   end
   print arr
 end
-
-arr = [10, 5, 8, 11, 100, 8, 3]
-bubble_sort(arr)
 
 def bubble_sort_by(arr)
   arr.length.times do
@@ -20,8 +15,4 @@ def bubble_sort_by(arr)
     end
   end
   print arr
-end
-
-bubble_sort_by(%w[hiiiiii hello hey]) do |left, right|
-  left.length - right.length
 end
